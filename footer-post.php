cbfc7c29044797d06043602b5ba300fcd38d61bf
A. Dean Kelly <admin@superindie.io>
2018-02-04 12:39:23 -0500

    0.01.02
    Version 0.01.02

--- footer-post.php
+++ footer-post.php
@@ -52,7 +52,7 @@
     </script>
     
     <!--JQuery Code-->
-    <script src='<?php echo get_template_directory_uri(); ?>/js/jquery.min.js'></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     
     <!--Advertising.js-->
     <script type='text/javascript' src='<?php echo get_template_directory_uri(); ?>/js/advertising.js'></script>
