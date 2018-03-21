47f7ad733cde0b434561f525cd95645602e55960
A. Dean Kelly <admin@superindie.io>
2018-03-21 14:39:04 -0400

    Version 0.01.30
    Version 0.01.30

--- header-home.php
+++ header-home.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet"  type="text/css" href='<?php echo get_template_directory_uri(); ?>/style-home.css?r=<?php echo time(); ?>'/>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto:400, 900" rel="stylesheet">
     <meta name='viewport' content='initial-scale=1'/>
-    <link type="image/x-icon" rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+    <link type="image/x-icon" rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-96x96.ico" />
     <meta name='theme-color' content='#E5E5F2' />
     
     <!--Meta Info-->
--- header-post.php
+++ header-post.php
@@ -5,7 +5,7 @@
     
     <!--Styles-->
     <link rel="stylesheet"  type="text/css" href='<?php echo get_template_directory_uri(); ?>/style-post.css?r=<?php echo time(); ?>'/>
-    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-96x96.ico" />
     <link href="https://fonts.googleapis.com/css?family=Comfortaa|Open+Sans|Roboto:400, 900" rel="stylesheet">
     <meta name='viewport' content='initial-scale=1'/>   
     <meta name='theme-color' content='#E5E5F2' />
